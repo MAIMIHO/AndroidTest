8a44fa6d92c198b1e595a041ebe9161b19e94c65
罗帅 <178257442@qq.com>
2017-02-11 20:50:34 +0800

    README.md

--- app/src/main/java/com/maimiho/androidtest/activity/MainActivity.java
+++ app/src/main/java/com/maimiho/androidtest/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.maimiho.androidtest;
+package com.maimiho.androidtest.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.maimiho.androidtest.R;
 
 public class MainActivity extends AppCompatActivity {
 
